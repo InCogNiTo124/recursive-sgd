e37a208bc9e1fa9884e733b9a0a0e53c6cc926fd
InCogNiTo124 <marijansmetko123@gmail.com>
2020-01-21 22:21:11 +0100

    Add testing mode

--- cli.py
+++ cli.py
@@ -16,7 +16,7 @@ class SGDConfig:
     def __repr__(self):
         return repr(self.__dict__)
 
-def parse(arguments):
+def parse_train(arguments):
     config = SGDConfig()
     config.to_shuffle = True
     i = 0
@@ -61,10 +61,31 @@ def parse(arguments):
         i += 1
     return config
 
+METRICS = {'accuracy': lambda y_true, y_pred: np.mean(y_true == y_pred)}
+
+def parse_test(arguments):
+    config = SGDConfig()
+    i = 0
+    while i < len(arguments):
+        arg = arguments[i]
+        if arg in ['-m', '--model']:
+            i += 1
+            config.model = arguments[i]
+        elif arg in ['-d', '--dataset']:
+            i += 1
+            config.dataset = arguments[i]
+        elif arg in ['--metrics']:
+            i += 1
+            config.metrics = METRICS[arguments[i]]
+        elif arg in ['-i', '--input-size']:
+            i += 1
+            config.input_size = int(arguments[i])
+        i += 1
+    return config
+
 def main():
-    print(sys.argv)
     if sys.argv[1] == 'train':
-        config = parse(sys.argv[2:])
+        config = parse_train(sys.argv[2:])
         with open(config.dataset, 'r') as file:
             dataset = np.array(list(csv.reader(file, delimiter=',')), dtype=np.float64)
             X_train = dataset[:, :config.input_size]
@@ -72,6 +93,15 @@ def main():
         sgd.train(X_train, y_train, MODEL, config.loss, batch_size=config.batch_size,
                   epochs=config.epochs, shuffle=config.to_shuffle, lr=config.lr)
         sgd.save(MODEL, 'MODEL.sgd')
+    if sys.argv[1] == 'test':
+        config = parse_test(sys.argv[2:])
+        model = sgd.load(config.model)
+        with open(config.dataset, 'r') as file:
+            dataset = np.array(list(csv.reader(file, delimiter=',')), dtype=np.float64)
+            X_test = dataset[:, :config.input_size]
+            y_test = dataset[:, config.input_size:]
+        print(config.metrics(y_test.flatten(), (model.forward(X_test).flatten() > 0.5).astype(np.float64)))
+
     return
 
 if __name__ == '__main__':
--- recursive_sgd/__init__.py
+++ recursive_sgd/__init__.py
@@ -1,2 +1,3 @@
 from recursive_sgd.save import save as save
+from recursive_sgd.save import load as load
 from recursive_sgd.sgd import sgd as train
