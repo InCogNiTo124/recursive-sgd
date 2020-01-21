f39ec1e1e2c148c1cdf6ef9fb0a7a52917e7382c
InCogNiTo124 <marijansmetko123@gmail.com>
2020-01-22 00:49:24 +0100

    New version

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import setuptools
 
-long_description=""
+with open('README.md', 'r') as file:
+    long_description = file.read()
 
 setuptools.setup(
     name="recursive_sgd",
-    version="0.4.0",
+    version="0.4.2",
     author="Marijan Smetko",
     author_email="marijan.smetko@gmail.com",
     description="Train a neural network - with recursion!",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/InCogNiTo124/recursive-sgd/',
+    entry_points={
+        'console_scripts': ['sgd=recursive_sgd.cli:main'],    
+    },
     packages=setuptools.find_packages(),
     classifiers=[],
-    python_requires='>=3.4'
+    python_requires='>=3.4',
+    install_requires=['numpy']
 )
